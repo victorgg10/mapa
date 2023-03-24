2ca1f8569a8d46a7eebb6d452bd5c0109d8ed4d5
Victor <garciavictorm997@gmail.com>
2023-03-24 02:23:05 -0700

    fix event

--- js.js
+++ js.js
@@ -7,11 +7,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([19.43924970413274, -99.12988803804318]).addTo(map);
-var marker2 = L.marker([19.38049767942047, -99.14115058861158]).addTo(map);
-
-marker.bindPopup("<b>yo no vivo aqui!</b><br> que tranza gris.").openPopup();
-marker2.bindPopup("<b>walmart talplan!</b><br>wenarda.").openPopup();
 
 const getData = () =>{
     fetch('/tiendas.json')
@@ -28,7 +23,9 @@ const getData = () =>{
             marker2 = L.marker([coor.geometry.coordinates[1], coor.geometry.coordinates[0]]).addTo(map);
             marker2.bindPopup(`<b>${coor.properties.institucion}</b><br>${coor.properties.tipo}.`).openPopup();
         }*/
-    } ).catch( error => {error})
+    } ).catch((error) => {
+        console.error("Error:", error);
+      });
 };
 
 getData();
